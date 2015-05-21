8ace1bd2e02021878e7a6e5163d8725e1cf16033
Ian Bellamy <bellamy.ian.m@gmail.com>
2015-05-20 22:24:16 -0600

    example location object

--- main.py
+++ main.py
@@ -12,13 +12,19 @@ def create_tables(test=False):
 def get_location(address):
 	# https://developers.google.com/maps/documentation/geocoding/
 	# https://github.com/googlemaps/google-maps-services-python
+	
+	# takes the tuple, (1115, N, Logan, ST)
+	# and morphs it into an array/list
 	address = list(address)
 	searchfor = ""
 	for word in address:
+		# for each item in that list concatenates to string
 		searchfor += word + " "
+	# takes that string and calls the Maps API
 	gmaps = googlemaps.Client(key=GMAPS_API)
 	geo_result = gmaps.geocode(searchfor + "Denver, CO")
 	# TODO parse geo_result
+	# location should be found here
 	return geo_result
 
 	
